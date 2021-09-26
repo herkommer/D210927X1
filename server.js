24ee78fea666a94de322ffb5d943a037a0b86674
herkommer <m@herkommer.co.uk>
2021-09-26 13:33:44 +0200

    added serving index.html using the fs module

--- server.js
+++ server.js
@@ -3,17 +3,24 @@
 // Set access to the http module (provided by node)
 const http = require('http');
 
+// Set access to the fs (file system) module
+const fs = require('fs');
+
 // Create a local server that responds & sends data back to the requestor (browser)
 const server = http.createServer();
 
 // Declare a function for what happens when the (incoming) request event triggers
 server.on('request', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!');
+    fs.readFile('index.html',function (err, data){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.setHeader('Content-Length', data.length);
+        res.write(data);
+        res.end();
+    });
 });
 
 // Have the server start listening locally to the port 8042
 server.listen(8042);
 
-console.log('Server up and listening on port 8042')
+console.log('Server up and listening on port 8042');
